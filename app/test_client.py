412faf2edc348871ecf4439a0dbeef53bf7285fd
acoderwannabe19 <acoderwannabe19>
2024-05-17 23:48:07 +0000

    Adding the bot

--- app/test_client.py
+++ app/test_client.py
@@ -1,18 +1,18 @@
 from langserve import RemoteRunnable
 
-ollama_llm = RemoteRunnable("http://localhost:8000/bot/")
+llm = RemoteRunnable("http://localhost:8000/bot/")
 
 
-# resp = ollama_llm.invoke({"question": "quel est le nombre de transactions de type P2P?"},
+# resp = llm.invoke({"question": "quel est le nombre de transactions de type P2P?"},
 #                         config={"configurable": {"session_id": ""}},
 # ) 
-# resp = ollama_llm.invoke({"question": "quel est le nombre de transactions a l'utilisateur avec l'id 18222 etait le destinataire?"},
+# resp = llm.invoke({"question": "quel est le nombre de transactions a l'utilisateur avec l'id 18222 etait le destinataire?"},
 #                         config={"configurable": {"session_id": ""}},
 # ) 
-# resp = ollama_llm.invoke({"question": "le montant total de transaction pour le type CASHIN  et CASHOUT?"},
+# resp = llm.invoke({"question": "le montant total de transaction pour le type CASHIN  et CASHOUT?"},
 #                         config={"configurable": {"session_id": ""}},
 # ) 
 
-resp = ollama_llm.invoke({"question": "quel sont les types de fraude en fintech?"},
+resp = llm.invoke({"question": "quel sont les types de fraude en fintech?"},
                         config={"configurable": {"session_id": ""}},)
-print(resp.content)
+print(resp)
--- streamlit/pages/2_🤖_Bot.py
+++ streamlit/pages/2_🤖_Bot.py
@@ -1,7 +1,39 @@
 import streamlit as st
 
+from langserve import RemoteRunnable
+
+llm = RemoteRunnable("http://localhost:8000/bot/")
+
 st.set_page_config(
   page_title='Bot',
   layout='wide',
   page_icon='📜'
 )
+
+# # Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# # Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+        
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    
+resp = llm.invoke({"question": prompt},
+                        config={"configurable": {"session_id": "123"}},)
+    
+response = resp.content
+# print(prompt)
+# Display assistant response in chat message container
+with st.chat_message("assistant"):
+    st.markdown(response)
+# Add assistant response to chat history
+st.session_state.messages.append({"role": "assistant", "content": response})
