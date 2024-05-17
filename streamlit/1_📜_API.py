9d8e498b80802f259c50827b9764e58df59f6a07
Mouhamadou Naby DIA <dnaby@ept.sn>
2024-05-17 22:22:05 +0000

    feat: setup streamlit

---
+++ streamlit/1_📜_API.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+  page_title='API',
+  layout='wide',
+  page_icon='📜'
+)
+
+st.sidebar.image('https://www.creativefabrica.com/wp-content/uploads/2021/07/05/Fraud-Detection-icon-Graphics-14301492-1.jpg')
+
+st.title('Detection de fraudes dans la fintech')
+st.divider()
---
+++ streamlit/pages/2_🤖_Bot.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(
+  page_title='Bot',
+  layout='wide',
+  page_icon='📜'
+)
